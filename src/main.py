f7862021948d4f21b7300f1c71a85e7054fcd943
stan-zapalikov <stanislavzapalikov@pm.me>
2025-01-07 00:01:51 -0800

    Started work on the copy_static() function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 from .textnode import TextNode, TextType
 from .htmlnode import HTMLNode
 from .leafnode import LeafNode
@@ -6,5 +7,8 @@ from .parentnode import ParentNode
 def main():
     pass
 
+def copy_static(path):
+    if (os.path.exists(path)):
+        pass
 
 main()
