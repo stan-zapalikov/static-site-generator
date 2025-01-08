4b444adac290cb3bd3b8efaf9b88757ac43113a9
stan-zapalikov <stanislavzapalikov@pm.me>
2025-01-07 17:20:16 -0800

    UNTESTED: completed the copy_static(,) function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,25 @@
 import os
+import shutil
 from .textnode import TextNode, TextType
 from .htmlnode import HTMLNode
 from .leafnode import LeafNode
 from .parentnode import ParentNode
 
 def main():
-    pass
+    if (os.path.exists('public')):
+        shutil.rmtree('public')
+    os.mkdir('public')
 
-def copy_static(path):
-    if (os.path.exists(path)):
-        pass
+    copy_static('static', 'public')
+
+def copy_static(source, destination):
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+        if os.path.isdir(source_path):
+            os.mkdir(dest_path)
+            copy_static(source_path, dest_path)
 
 main()
